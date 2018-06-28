239ba2cd4ba7fcfefeb244cfb89c44082e941cb2
J. Max Wilson <jmaxwilson@gmail.com>
2018-06-28 13:48:13 -0600

    change .html to .php and show phpinfo

--- webserver1/html/index.php
+++ webserver1/html/index.php
@@ -2,10 +2,11 @@
 	<head>
 		<title>web1.localhost</title>
 	</head>
-	<body>
+	<body style="text-align: center">
 		<h1>Welcome to WebServer 1</h1>
 		<p>
 			This content is being served by the webserver1 container
 		</p>
+		<? phpinfo(); ?>
 	</body>
 </html>
--- webserver2/html/index.php
+++ webserver2/html/index.php
@@ -2,10 +2,11 @@
 	<head>
 		<title>web2.localhost</title>
 	</head>
-	<body>
+	<body style="text-align: center">
 		<h1>Welcome to WebServer 2</h1>
 		<p>
 			This content is being served by the webserver2 container
 		</p>
+		<? phpinfo(); ?>
 	</body>
 </html>
